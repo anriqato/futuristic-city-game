31743d71ae05f2373f3a7bd894b9e7c52adc549b
anriqato <anriqato@yahoo.de>
2025-03-13 08:14:14 +0100

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FuturisticCity from './components/FuturisticCity';
+
+function App() {
+  return (
+    <div className="App">
+      <FuturisticCity />
+    </div>
+  );
+}
+
+export default App;
